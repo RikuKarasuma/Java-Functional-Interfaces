ddf07e35eb0b6eefede59ea4510b19aaa3519d68
rikukarasuma <owenmcmonagle@ymail.com>
2020-01-20 17:31:04 +0000

    Implemented BiConsumer exampleImplemented BiFunction exampleImplemented BinaryOperator example

---
+++ src/xyz/softwareeureka/practice/functional/FunctionalInterfacesPractice.java
@@ -0,0 +1,63 @@
+package xyz.softwareeureka.practice.functional;
+
+import static java.lang.System.out;
+
+// BiConsumer interface takes two arguments for processing.
+import java.util.function.BiConsumer;
+
+// BiFunction interface takes two arguments and produces a result.
+import java.util.function.BiFunction;
+
+// BinaryOperator interface takes two arguments and produces a result of the 
+// same type as opposed to BiFunction which takes two arguments and produces a 
+// result of a different type.
+import java.util.function.BinaryOperator;
+
+public class FunctionalInterfacesPractice
+{
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+		/**
+		 * A BiConsumer interface accepts dual arguments.
+		 */
+		BiConsumer<Integer, Boolean> dual_args_literal_function = (number_to_process, process_now) -> {
+			if(process_now)
+				out.println("(BiConsumer)Processed number:" + (number_to_process *= 2));
+		};
+		
+		// Execute our dual argument functional interfaces.
+		dual_args_literal_function.accept(20, false);
+		dual_args_literal_function.accept(20, true);
+		
+		
+		/**
+		 * A BiFunction interface accepts dual arguments in order to produce 
+		 * a result.
+		 */
+		BiFunction<Integer, Integer, Integer> dual_args_for_result_literal_function = (number_to_process, multiplier) -> 
+			{return number_to_process * multiplier;};
+			
+		// Execute our dual argument functional interface and receive the result.
+		final int result = dual_args_for_result_literal_function.apply(10, 100);
+		out.println("(BiFunction)Returned processed number:" + result);
+		
+		/**
+		 * A BinaryConsumer interface accepts Two arguments however it only accepts
+		 * a single type argument. This means each argument and the result must be of
+		 * same type. 
+		 */
+		BinaryOperator<Double> dual_args_for_result_without_result_type = (number_to_process, multipler) ->
+			{return number_to_process * multipler;};
+		
+		// Execute our dual argument functional interface and retrieve the result.
+		final double result_of_binary_operator = dual_args_for_result_without_result_type.apply(30d, 2.5d);
+		out.println("(BinaryOperator)Returned processed number:" + result_of_binary_operator);
+		
+		
+	}
+}
